d1e061791da890fa0f8fdb7f66a7975c742d16b7
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-02-10 20:48:29 +0000

    add custom config to datalink channel

--- src/scan/syn.rs
+++ src/scan/syn.rs
@@ -1,10 +1,20 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, time::Duration};
 
-use pnet::datalink::channel;
+use once_cell::sync::Lazy;
+use pnet::datalink::{channel, ChannelType, Config};
 
 use crate::{abort, error::ScanError, interface};
 
 use super::{Executor, PortState};
+
+const TIMEOUT: Duration = Duration::from_millis(1500);
+
+static CONFIG: Lazy<Config> = Lazy::new(|| Config {
+    read_timeout: TIMEOUT.into(),
+    write_timeout: TIMEOUT.into(),
+    channel_type: ChannelType::Layer2, // Default type, but I want to make sure.
+    ..Default::default()
+});
 
 #[derive(Debug)]
 pub struct Scan;
@@ -12,7 +22,7 @@ pub struct Scan;
 impl Executor for Scan {
     fn scan(&self, addr: &SocketAddr) -> PortState {
         use pnet::datalink::Channel::Ethernet;
-        let (tx, rs) = match channel(&interface::DEFAULT, Default::default()) {
+        let (tx, rs) = match channel(&interface::DEFAULT, *CONFIG) {
             Ok(Ethernet(tx, rx)) => (tx, rx),
             // INFO: make sure that ethernet is the only datalink channel available.
             Ok(_) => unreachable!(),
