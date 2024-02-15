26c99b7c99efc24104baed17b8b9635befc9fa4d
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-02-15 16:40:00 +0000

    fix typos in error messages

--- src/error.rs
+++ src/error.rs
@@ -4,30 +4,30 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum ScanError {
-    #[error("port `{0}` is invalid")]
+    #[error("Port `{0}` is invalid")]
     InvalidPort(String),
-    #[error("failed to check target kind (ensure it's a domain or IPv4)")]
+    #[error("Failed to check target kind (ensure it's a domain or IPv4). Origin: {0}")]
     HostParseFailed(#[source] url::ParseError),
-    #[error("failed to resolve the given target: {0}")]
+    #[error("Failed to resolve the given target. Origin: {0}")]
     ResolverFailed(#[source] std::io::Error),
-    #[error("resolver didn't find any IPv4 address mapped by `{0}`")]
+    #[error("Resolver didn't find any IPv4 address mapped by. Origin: `{0}`")]
     DomainLookupFailed(String),
-    #[error("you must run the scanner as sudo")]
+    #[error("You must run the scanner as sudo")]
     NormalUserRequired,
-    #[error("no network interfaces available")]
+    #[error("No network interfaces available")]
     MissingDefaultInterface,
-    #[error("failed to create socket: {0}")]
+    #[error("Failed to create socket. Origin: {0}")]
     DatalinkChannelFailed(#[source] std::io::Error),
-    #[error("make sure the default network interface has an IPv4")]
+    #[error("Make sure the default network interface has an IPv4")]
     OnlyIpv4InterfaceSupported,
-    #[error("only supports IPv4 addresses or domains that map addresses with this IP version")]
+    #[error("Only supports IPv4 addresses or domains that map addresses with this IP version")]
     OnlyIpv4TargetSupported,
-    #[error("failed to get MAC address of gateway: {0}")]
+    #[error("Failed to get MAC address of gateway. Origin: {0}")]
     GatewayLookupFailed(String),
-    #[error("failed to get MAC address")]
+    #[error("Failed to get MAC address")]
     MissingMacAddr,
-    #[error("failed to send packet to `{0}`: {1}")]
+    #[error("Failed to send packet to `{0}`. Origin: {1}")]
     PacketSendFailed(IpAddr, #[source] std::io::Error),
-    #[error("failed to receive packet to `{0}`: {1}")]
+    #[error("Failed to receive packet to `{0}`. Origin: {1}")]
     PacketRecvFailed(IpAddr, #[source] std::io::Error),
 }
